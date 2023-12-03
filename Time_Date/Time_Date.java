c6ff79c235bd5f3fcb2560e0ce26d824e005b996
Pavan Koppula <145316378+pavan-koppula@users.noreply.github.com>
2023-12-03 12:17:07 +0530

    Add files via upload

---
+++ Time_Date/Time_Date.java
@@ -0,0 +1,18 @@
+package Time_Date;
+
+public class Time_Date {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		System.out.println(java.time.LocalTime.now());
+		System.out.println(java.time.LocalDate.now());
+		System.out.println(java.time.LocalDateTime.now());
+		
+		  java.util.Date date = new java.util.Date();    
+		    System.out.println(date);
+		
+	}
+
+}
